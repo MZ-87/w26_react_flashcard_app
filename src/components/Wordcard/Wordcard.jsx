75253904948d2d0bad14587382a4d7f78f9e9c8d
MZ-87 <zsmile@mail.ru>
2022-12-06 10:14:33 +0300

    try to make interactive card

--- src/components/Wordcard/Wordcard.jsx
+++ src/components/Wordcard/Wordcard.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../Button/Button'
 import style from './wordcard.module.scss'
 
 export default function Wordcard(
   props) {
-  //показываем перевод
-  function showTranslation() {
-    return <h3 className={style.translate}>{props.translate}</h3>
-  }
-  //показываем кнопку
-  function showTranslateBtn() {
-    <Button text={'Перевод'} />
+  // показываем перевод
+  const showTranslation = () => {
+    return (
+      <div>
+        <h3 className={style.translate}>{props.translate}</h3>
+        <Button text={'Скрыть перевод'} onClick={hideTranslation} />
+      </div>
+    )
   }
 
-  let actionItem;
-  if (clicked) {
-    actionItem = <AddToCartForm addedToCart={addedToCart} />;
-  } else {
-    actionItem = <AddToCartButton />;
+  //показываем кнопку
+  const hideTranslation = () => {
+    <Button text={'Показать перевод'} />
   }
+
+  // const [pressed, setPressed] = useState(false);
+  // const handleClick = () => {
+  //   setPressed((prevState) => {
+  //     prevState = !prevState;
+  //   })
+  // }
+
+  // let actionItem;
+  // if (pressed) {
+  //   actionItem = <h3 className={style.translate}>{props.translate}</h3>;
+  // } else {
+  //   actionItem = <Button text={'Показать перевод'} pressed={pressed} setPressed={setPressed} onClick={handleClick} />;
+  // }
 
   return (
     <div className={style.main}>
@@ -28,7 +41,8 @@ export default function Wordcard(
           <h3 className={style.word}>{props.word}</h3>
           <h3 className={style.transcription}>{props.transcription}</h3>
           {/* <h3 className={style.translate}>{props.translate}</h3> */}
-          <Button text={'Перевод'} />
+          <Button text={'Показать перевод'} onClick={showTranslation} />
+          {actionItem}
         </div>
         <button className={style.next_btn}><img src="./assets/arrow_forward_icon.png" alt="arrow forward" className={style.nav_btn} /></button>
       </div>
