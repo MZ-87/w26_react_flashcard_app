bcec7a35752cb160f63d3fefcb29dbf26026850e
MZ-87 <zsmile@mail.ru>
2022-12-06 14:42:08 +0300

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,24 +2,22 @@ import React from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 // import Error from '../Error/Error'
-import Wordcard from '../Wordcard/Wordcard'
-// import Wordlist from '../Wordlist/Wordlist'
-// import Button from '../Button/Button'
-// import words from '../data/words.json'
+import ListHeader from '../ListHeader/ListHeader'
+import Wordlist from '../Wordlist/Wordlist'
+import Button from '../Button/Button'
+import words from '../data/words.json'
 import style from './app.module.scss'
 
 function App() {
   return (
     <div className={style.App}>
       <Header />
-      {/* <Banner /> */}
       {/* <Error /> */}
-      {/* <ListHeader />
-      <Button text={'Добавить слово'} />
+      <ListHeader />
+      <div className={style.addBtn}><Button text={'Добавить слово'} /></div>
       {words.map((word) => {
         return <Wordlist key={word.id} word={word.word} translate={word.translate} transcription={word.transcription} tag={word.tag} />
-      })} */}
-      <Wordcard word={'abdomen'} transcription={'[ˈæbdəmən]'} translate={'живот'} />
+      })}
       <Footer />
     </div>
   );
