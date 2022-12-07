8e9e9ab38160b4ebc6793f263c927f4b902ef419
MZ-87 <zsmile@mail.ru>
2022-12-07 16:25:35 +0300

    button Add works

--- src/components/AddWord/AddWord.jsx
+++ src/components/AddWord/AddWord.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
-import BtnAdd from '../BtnAdd/BtnAdd'
+import React, { useState } from 'react'
+// import BtnAdd from '../BtnAdd/BtnAdd'
+import Button from '../Button/Button'
 import style from './addword.module.scss'
 
 export default function AddWord(props) {
+    const [pressed, setPressed] = useState(false)
+    const handleClick = () => {
+        setPressed(!pressed);
+    };
+
     return (
         <div className={style.container}>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div className={style.buttons}>
-                <div className={style.pseudoBtn}></div>
-                <BtnAdd />
-            </div>
-        </div >
+            {pressed ? (
+                <>
+                    <input type="text" className={style.input}></input>
+                    <input type="text" className={style.input}></input>
+                    <input type="text" className={style.input}></input>
+                    <input type="text" className={style.input}></input>
+                    <div className={style.buttons}>
+                        <Button text={'Сохранить'} />
+                        <Button text={'Отмена'} />
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div className={style.buttons}>
+                        <div className={style.pseudoBtn}></div>
+                        <button className={style.btn} onClick={handleClick}>Добавить</button>
+                    </div>
+                </>
+            )}
+        </div>
     )
 }
 
-// при нажатии на кнопку Добавить
-// eslint-disable-next-line
-{/* <div className={style.container}>
-            <input type="text" className={style.input}></input>
-            <input type="text" className={style.input}></input>
-            <input type="text" className={style.input}></input>
-            <input type="text" className={style.input}></input>
-            <div className={style.buttons}>
-                <Button text={'Сохранить'} />
-                <Button text={'Отмена'} />
-            </div>
-        </div > */}
