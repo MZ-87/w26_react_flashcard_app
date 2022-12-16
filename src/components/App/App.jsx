7bfa84ed71c972ab409472c7770b3c49e471f6c3
MZ-87 <mariya.zeleneva@gmail.com>
2022-12-16 19:59:56 +0300

    fix problemwith 2 elem in Route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,29 +10,30 @@ import Footer from '../Footer/Footer'
 import Banner from '../Banner/Banner'
 import Error from '../Error/Error'
 import Wordcard from '../Wordcard/Wordcard'
-// import ListHeader from '../ListHeader/ListHeader'
+import ListHeader from '../ListHeader/ListHeader'
 import Wordlist from '../Wordlist/Wordlist'
 import Button from '../Button/Button'
-// import words from '../data/words.json'
+import words from '../data/words.json'
 import style from './app.module.scss'
 
 function App() {
+  const List = () => {
+    return (
+      <>
+        <ListHeader />
+        {words.map((word) => {
+          return <Wordlist key={word.id} word={word.word} translate={word.translate} transcription={word.transcription} tag={word.tag} />
+        })}
+      </>
+    )
+  }
   return (
     <Router>
       <div className={style.App}>
         <Header />
-        {/* <Banner /> */}
-        {/* <Error /> */}
-        {/* <ListHeader />
-      <Button text={'Добавить слово'} />
-      {words.map((word) => {
-        return <Wordlist key={word.id} word={word.word} translate={word.translate} transcription={word.transcription} tag={word.tag} />
-      })} */}
-        {/* <Wordcard word={'abdomen'} transcription={'[ˈæbdəmən]'} translate={'живот'} /> */}
-
         <Routes>
           <Route path="/" element={<Banner />}></Route>
-          <Route path="/wordlist" element={<Wordlist />}>Список слов</Route>
+          <Route path="/wordlist" element={<List />}>Список слов</Route>
           <Route path="/cards" element={<Wordcard word={'abdomen'} transcription={'[ˈæbdəmən]'} translate={'живот'} />}>Карточки</Route>
           <Route path='/training'>Тренировка</Route>
           <Route path="*" element={<Error />}></Route>
