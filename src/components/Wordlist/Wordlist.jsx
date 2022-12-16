2fcb89e9f0d7fe83ad991e8c3dc638094585e350
MZ-87 <mariya.zeleneva@gmail.com>
2022-12-16 12:28:12 +0300

    update Wordlist component

--- src/components/Wordlist/Wordlist.jsx
+++ src/components/Wordlist/Wordlist.jsx
@@ -11,32 +11,39 @@ export default function Wordlist(props) {
     const handleCancel = () => {
         setIsEditMode(!isEditMode);
     }
+    const SaveCancelBtns = () => {
+        return (
+            <>
+                <input className={style.input} placeholder={props.word}></input>
+                <input className={style.input} placeholder={props.translate}></input>
+                <input className={style.input} placeholder={props.transcription}></input>
+                <input className={style.input} placeholder={props.tag}></input>
+                <div className={style.buttons}>
+                    <button className={style.btn} >Сохранить</button>
+                    <button className={style.btn} onClick={handleCancel}>Отмена</button>
+                </div>
+            </>
+        )
+    }
+
+    const EditDeleteBtns = () => {
+        return (
+            <>
+                <div className={style.item}>{props.word}</div>
+                <div className={style.item}>{props.translate}</div>
+                <div className={style.item}>{props.transcription}</div>
+                <div className={style.item}>{props.tag}</div>
+                <div className={style.buttons}>
+                    <button className={style.btn} onClick={handleEditMode}>Редактировать</button>
+                    <button className={style.btn} >Удалить</button>
+                </div>
+            </>
+        )
+    }
 
     return (
         <div className={style.container}>
-            {isEditMode ? (
-                <>
-                    <input className={style.input} placeholder={props.word}></input>
-                    <input className={style.input} placeholder={props.translate}></input>
-                    <input className={style.input} placeholder={props.transcription}></input>
-                    <input className={style.input} placeholder={props.tag}></input>
-                    <div className={style.buttons}>
-                        <button className={style.btn} >Сохранить</button>
-                        <button className={style.btn} onClick={handleCancel}>Отмена</button>
-                    </div>
-                </>
-            ) : (
-                <>
-                    <div className={style.item}>{props.word}</div>
-                    <div className={style.item}>{props.translate}</div>
-                    <div className={style.item}>{props.transcription}</div>
-                    <div className={style.item}>{props.tag}</div>
-                    <div className={style.buttons}>
-                        <button className={style.btn} onClick={handleEditMode}>Редактировать</button>
-                        <button className={style.btn} >Удалить</button>
-                    </div>
-                </>
-            )}
+            {isEditMode ? <SaveCancelBtns /> : <EditDeleteBtns />}
         </div >
     )
 }
