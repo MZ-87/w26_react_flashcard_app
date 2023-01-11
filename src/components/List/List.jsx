f1e9fad7fb9796da90161459741b330630d5b194
MZ-87 <zsmile@mail.ru>
2023-01-11 15:38:19 +0300

    put footer in the end of page

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ListHeader from '../ListHeader/ListHeader'
 import Wordlist from '../Wordlist/Wordlist'
 import words from '../data/words.json'
-// import style from './app.module.scss'
+import style from './list.module.scss'
 
 export default function List() {
     return (
-        <>
+        <div className={style.container}>
             <ListHeader />
             {words.map(item => (
                 <Wordlist key={item.id} item={{ ...item }} />
             ))}
-        </>
+        </div>
     )
 }
