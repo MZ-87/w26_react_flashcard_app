49c72ce5c11f7aac8032f1a8e877a228ca146a8b
MZ-87 <zsmile@mail.ru>
2023-01-12 11:59:20 +0300

    add counter, doesn't work

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -7,48 +7,55 @@ import { useState, useEffect } from 'react'
 
 export default function Slider() {
     const [cards, setCards] = useState(false)
-    const [clicked, setClicked] = useState(false)
+    const [pressed, setPressed] = useState(false)
     const [cardIndex, setCardIndex] = useState(0)
+    const [learnedWords, setLearnedWords] = useState(0);
 
     useEffect(() => {
         setCards(words)
     }, [])
 
-
-    function prevCard() {
+    const showPrevCard = () => {
         let index = cardIndex;
 
         if (index !== 0) {
             index--;
             setCardIndex(index);
         } else if (index === 0) {
-            setCardIndex(cards.length - 1);
+            setCardIndex(0);
         }
-        setClicked(false)
+        setPressed(false)
     }
 
-    function nextCard() {
+    const showNextCard = () => {
         let index = cardIndex;
 
         if (index !== cards.length - 1) {
             index++;
             setCardIndex(index);
         } else if (index === cards.length - 1) {
-            setCardIndex(0);
+            setCardIndex(cards.length - 1);
         }
-        setClicked(false)
+        setPressed(false)
     }
+
+    const countWords = () => {
+        setLearnedWords(learnedWords + 1);
+    };
+    console.log(learnedWords);
 
     if (!cards) {
         return <h1>Loading...</h1>
     }
+
     return (
         <div className={style.main}>
             <div className={style.container}>
-                <button onClick={prevCard} className={style.prev_btn}><img src="./assets/arrow_back_icon.png" alt="arrow back" className={style.nav_btn} /></button>
+                <button onClick={showPrevCard} className={style.prev_btn}><img src="./assets/arrow_back_icon.png" alt="arrow back" className={style.nav_btn} /></button>
                 <Wordcard item={cards[cardIndex]} />
-                <button onClick={nextCard} className={style.next_btn}><img src="./assets/arrow_forward_icon.png" alt="arrow forward" className={style.nav_btn} /></button>
+                <button onClick={showNextCard} className={style.next_btn}><img src="./assets/arrow_forward_icon.png" alt="arrow forward" className={style.nav_btn} /></button>
             </div >
+            <div className={style.word_counter}>Выучено слов: {learnedWords}/{cards.length - 1}</div>
         </div >
     )
 }
