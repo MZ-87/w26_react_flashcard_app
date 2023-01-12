bebab1ce91997ab0b72bbc502e0e4481fa79d009
MZ-87 <zsmile@mail.ru>
2023-01-12 16:03:06 +0300

    delete ListHeader, add it's code to List

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import ListHeader from '../ListHeader/ListHeader'
 import Wordlist from '../Wordlist/Wordlist'
 import words from '../data/words.json'
 import style from './list.module.scss'
@@ -7,7 +6,13 @@ import style from './list.module.scss'
 export default function List() {
     return (
         <div className={style.container}>
-            <ListHeader />
+            <ul className={style.list_container}>
+                <li className={style.item}> Слово</li>
+                <li className={style.item}>Перевод</li>
+                <li className={style.item}>Транскрипция</li>
+                <li className={style.item}>Категория</li>
+                <li className={style.item}></li>
+            </ul >
             {words.map(item => (
                 <Wordlist key={item.id} item={{ ...item }} />
             ))}
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// eslint-disable-next-line
 import words from '../data/words.json'
 import style from './quiz.module.scss'
 
